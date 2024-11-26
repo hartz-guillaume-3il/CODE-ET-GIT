ff2d420ef0aebd9393df1104c9f8a4867df60a37
hartz-guillaume-3il <hartzg@3il.fr>
2024-11-26 16:34:22 +0100

    Exercice 3 : mis au format KISS et DRY

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,43 @@
 public class StockManager {
 
-    // gere le stock
-    public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
-        } else {
-            System.out.println("Opération inconnue.");
-        }
-    }
+	/**
+	 * Fonction gererStock qui permet de gérer les stocks à l'aide d'un switch case
+	 * 
+	 * @param typeOperation Type de l'opération à effectuer ("ajout" ou "retrait").
+	 * @param produit       Nom du produit concerné.
+	 * @param quantite      Quantité à ajouter ou retirer du stock.
+	 * @param stock         Stock actuel du produit.
+	 */
+	public void gererStock(String typeOperation, String produit, int quantite, int stock) {
+		switch (typeOperation.toLowerCase()) {
+		case "ajout":
+			stock += quantite;
+			afficherStock(produit, stock, "après ajout");
+			break;
+
+		case "retrait":
+			if (stock >= quantite) {
+				stock -= quantite;
+				afficherStock(produit, stock, "après retrait");
+			} else {
+				System.out.println("Stock insuffisant pour le produit : " + produit);
+			}
+			break;
+
+		default:
+			System.out.println("Opération inconnue.");
+		}
+	}
+
+	/**
+	 * Fonction d'affichage des stocks Fait appel à tous les éléments après leur
+	 * passage dans gererStock
+	 * 
+	 * @param produit   Nom du produit concerné.
+	 * @param stock     Stock actuel après l'opération.
+	 * @param operation Description de l'opération effectuée.
+	 **/
+	private void afficherStock(String produit, int stock, String operation) {
+		System.out.println("Produit : " + produit + ", Stock " + operation + " : " + stock);
+	}
 }
