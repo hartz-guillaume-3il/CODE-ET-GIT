a5647a1b355846be5c3cd8a9aa6cdfa29b35f6ca
hartz-guillaume-3il <hartzg@3il.fr>
2024-11-26 16:18:18 +0100

    Exercice 2 : mis au format KISS et DRY

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,39 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
-        System.out.println("Notes de " + nomEtudiant + ":");
-        for (int note : notes) {
-            System.out.println("- " + note);
-        }
+	/**
+	 * Affiche note et moyennes
+	 * Appel les fonctions afficherNotes et calculerEtAfficherMoyenne
+	 * */
+	public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
+		// Affichage des notes
+		afficherNotes(nomEtudiant, notes);
 
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
-            somme += note;
-        }
+		// Calcul et affichage de la moyenne
+		calculerEtAfficherMoyenne(notes);
+	}
 
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
-    }
+	/**
+	 * Affichage des notes
+	 * Parcour la liste de note pour les afficher en fonction du nom de l'étudiant
+	 * */
+	public void afficherNotes(String nomEtudiant, int[] notes) {
+		System.out.println("Notes de " + nomEtudiant + ":");
+		for (int note : notes) {
+			System.out.println("- " + note);
+		}
+	}
+
+	/** 
+	 * Calcul et affichage de la moyenne
+	 * Initialisation de la somme pour la moyenne puis addition des notes
+	 * Calcul de la moyenne en parcourant la liste note et en utilisatiant la somme
+	 * */
+	public void calculerEtAfficherMoyenne(int[] notes) {
+		int somme = 0;
+		for (int note : notes) {
+			somme += note;
+		}
+		double moyenne = (double) somme / notes.length;
+		System.out.println("Moyenne : " + moyenne);
+	}
 }
